350a3eb82248a6f189b70a3c4f05c0cf6220552a
gauravpathak <gauravpathak.spring@gmail.com>
2018-12-02 15:10:14 +0530

    Initial Commit.

---
+++ src/main/java/com/gaurav/springpetclinic/SpringpetclinicApplication.java
@@ -0,0 +1,12 @@
+package com.gaurav.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringpetclinicApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gaurav/springpetclinic/SpringpetclinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gaurav.springpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringpetclinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
