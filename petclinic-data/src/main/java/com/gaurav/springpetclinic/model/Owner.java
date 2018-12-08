16541bc24bb728820bac75e61a0595b56663d189
gauravpathak <gauravpathak.spring@gmail.com>
2018-12-08 17:40:01 +0530

    Added Visit Entity and updated Owner with Pet Relationship. Closes #26.

--- petclinic-data/src/main/java/com/gaurav/springpetclinic/model/Owner.java
+++ petclinic-data/src/main/java/com/gaurav/springpetclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package com.gaurav.springpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
