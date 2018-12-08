d041f12b88f1a0515fb46ebd0418de3998f98351
gauravpathak <gauravpathak.spring@gmail.com>
2018-12-08 17:45:41 +0530

    Added Speciality Entity and added the relationship of it with VET Entity. Closes #27.

---
+++ petclinic-data/src/main/java/com/gaurav/springpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.gaurav.springpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- petclinic-data/src/main/java/com/gaurav/springpetclinic/model/Vet.java
+++ petclinic-data/src/main/java/com/gaurav/springpetclinic/model/Vet.java
@@ -1,4 +1,14 @@
 package com.gaurav.springpetclinic.model;
 
 public class Vet extends Person{
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
