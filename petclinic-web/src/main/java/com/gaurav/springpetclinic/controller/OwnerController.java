536bf052cde9a39437dc13f028056e1376e203c4
gauravpathak <gauravpathak.spring@gmail.com>
2019-03-03 01:27:56 +0530

    Added the Web Data Binder

--- petclinic-web/src/main/java/com/gaurav/springpetclinic/controller/OwnerController.java
+++ petclinic-web/src/main/java/com/gaurav/springpetclinic/controller/OwnerController.java
@@ -3,7 +3,9 @@ package com.gaurav.springpetclinic.controller;
 import com.gaurav.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,6 +18,12 @@ public class OwnerController {
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder webDataBinder){
+        webDataBinder.setDisallowedFields("id");
+    }
+
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model) {
